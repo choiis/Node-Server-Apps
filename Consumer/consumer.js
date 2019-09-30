f69ae60dc43748e7b5b0ed719b46b7ce94c4c52a
최인성 <choiis1207@gmail.com>
2019-09-30 22:41:04 +0900

    20190930 Kafka Consumer Oracle 연결

--- Consumer/consumer.js
+++ Consumer/consumer.js
@@ -6,22 +6,58 @@ var Consumer = kafka.Consumer;
 var client = new kafka.KafkaClient();
 
 var  consumer = new Consumer(client, 
-	[ { topic: "errorLogs", partition: 0 }], {
+	[ { topic: "errorLogs", partition: 0 },
+		{ topic: "serverLog", partition: 0 }], {
 	autoCommit: true,
 	autoCommitIntervalMs: 5000,
 	encoding: 'utf8'
 });
 
-consumer.on("message", function(message) {
-	if(message.topic === "errorLogs") {
-		console.log(message.value);	
+var oracledb = require('oracledb');
+oracledb.autoCommit = true;
+
+var fs = require('fs'); // 파일목록 탐색
+var conn;
+
+fs.readFile('oracle.properties', 'utf8', (err, data) => {
+	var json = JSON.parse(data);
+	var config = {
+		user: json.user,
+		password: json.password,
+		connectString: json.connectString
+	};
+
+	// Global connection으로 만듬
+	oracledb.getConnection(config, (err, con) => {
+		if(err == null) {
+			console.log("Oracle connection success");
+			conn = con;
+		}
+	});
+});
+
+
+consumer.on("message", (message) => {
+	if(message.topic === "errorLogs") { // log from spring server
+		console.log("errorLogs : " + message.value);	
+		if(conn != null) {
+			//쿼리문 실행 
+	        conn.execute(message.value, (err,result) => {
+	            if(err) {
+	                console.log(err);
+	        	}
+	        });	
+		}
+
+	} else if(message.topic === "serverLogs") { // log from cpp server
+		console.log("serverLogs : " + message.value);	
 	}
 });
 
-consumer.on('error', function (err) {
+consumer.on('error', (err) => {
 	console.log('error:', err);
 });
 
-consumer.on('offsetOutOfRange', function (err) {
+consumer.on('offsetOutOfRange', (err) => {
     console.log('offsetOutOfRange:',err);
 });
