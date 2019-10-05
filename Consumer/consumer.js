06104460533abe14fa7340feebba0f9fbaa8648a
최인성 <choiis1207@gmail.com>
2019-10-06 00:09:05 +0900

    20191006 Kafka Consumer js 내용 분리

--- Consumer/consumer.js
+++ Consumer/consumer.js
@@ -4,53 +4,24 @@
 var kafka = require("kafka-node");
 var Consumer = kafka.Consumer;
 var client = new kafka.KafkaClient();
+var sqlserver = require('./sqlserver');
+var oracle = require('./oracle');
 
-var  consumer = new Consumer(client, 
+var consumer = new Consumer(client, 
 	[ { topic: "errorLogs", partition: 0 },
-		{ topic: "serverLog", partition: 0 }], {
+	{ topic: "serverLogs", partition: 0 }], {
 	autoCommit: true,
 	autoCommitIntervalMs: 5000,
 	encoding: 'utf8'
 });
 
-var oracledb = require('oracledb');
-oracledb.autoCommit = true;
-
-var fs = require('fs'); // 파일목록 탐색
-var conn;
-
-fs.readFile('oracle.properties', 'utf8', (err, data) => {
-	var json = JSON.parse(data);
-	var config = {
-		user: json.user,
-		password: json.password,
-		connectString: json.connectString
-	};
-
-	// Global connection으로 만듬
-	oracledb.getConnection(config, (err, con) => {
-		if(err == null) {
-			console.log("Oracle connection success");
-			conn = con;
-		}
-	});
-});
-
-
 consumer.on("message", (message) => {
 	if(message.topic === "errorLogs") { // log from spring server
 		console.log("errorLogs : " + message.value);	
-		if(conn != null) {
-			//쿼리문 실행 
-	        conn.execute(message.value, (err,result) => {
-	            if(err) {
-	                console.log(err);
-	        	}
-	        });	
-		}
-
+		oracle.insertSql(message.value);
 	} else if(message.topic === "serverLogs") { // log from cpp server
 		console.log("serverLogs : " + message.value);	
+		sqlserver.insertSql(message.value);
 	}
 });
 
---
+++ Consumer/sqlserver.js
@@ -0,0 +1,38 @@
+
+var mssql = require('mssql');
+var fs = require('fs'); // 파일목록 탐색
+
+fs.readFile('sqlserver.properties', 'utf8', function(err, data) {
+	var json = JSON.parse(data);
+
+	var config = {
+		user : json.user,
+		password : json.password,
+		server : json.server,
+		port : json.port,
+		database : json.database
+	};
+	// Global connection으로 만듬
+	mssql.connect(config).then(() => {
+		console.log("sqlserver connection success");
+	})
+	.catch((err) => {
+		console.log(err);
+	});;
+});
+
+var insertSql = ( async (sql) => {
+	// Query 
+	var request = new mssql.Request();
+	
+	let data = await request.query(sql).then(function() {
+		return 1;
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+	return data;
+});
+
+
+module.exports.insertSql = insertSql;
